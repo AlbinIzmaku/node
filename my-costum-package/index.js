571086d0d04204c897eb643409347d6b4ad2c8ad
Albin Izmaku <albinizmaku2@gmail.com>
2024-06-22 19:30:46 +0200

    new update

--- my-costum-package/index.js
+++
@@ -1,8 +0,0 @@
-const upperCase = require("upper-case").upperCase;
-
-function greet(name) {
-  console.log(upperCase(`Hello ${name}, welcome to Codevolution!`));
-}
-
-// greet("Albin");
-module.exports = greet;
--- new-package/index.js
+++
@@ -1,3 +0,0 @@
-const greet = require("my-costum-package");
-
-greet("Albin");
